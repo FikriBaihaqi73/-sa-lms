84f8f3b90a18d275993c1f623b5e702ef405f1d8
FikriBaihaqi73 <fikrimuhammad38993@gmail.com~>
2026-08-18 08:41:51 +0700

    feat:migaration

---
+++ packages/shared/src/prisma/migrations/20260818013011/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[name]` on the table `roles` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `name` to the `roles` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "roles" ADD COLUMN     "description" TEXT,
+ADD COLUMN     "name" VARCHAR NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "roles_name_key" ON "roles"("name");
