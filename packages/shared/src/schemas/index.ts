7803f5e44cf5f279544f709e9b7d6cde18c3fcaa
Hilmy Zaky Ramadhan <hilmyzaky028@gmail.com>
2026-09-01 09:07:57 +0700

    feat: create zod nationalities and institutions

--- packages/shared/src/schemas/index.ts
+++ packages/shared/src/schemas/index.ts
@@ -1,11 +1,25 @@
 export * from "#schemas/academic-status.schema";
+
 export * from "#schemas/academic-year.schema";
+
 export * from "#schemas/assignment-type.schema";
+
 export * from "#schemas/attendance-status.schema";
+
 export * from "#schemas/employment-status.schema";
+
 export * from "#schemas/institution-level.schema";
+
+export * from "#schemas/institution.schema";
+
+export * from "#schemas/nationality.schema";
+
 export * from "#schemas/permission.schema";
+
 export * from "#schemas/role.schema";
+
 export * from "#schemas/role-permission.schema";
+
 export * from "#schemas/specialization.schema";
+
 export * from "#schemas/user.schema";
---
+++ packages/shared/src/schemas/nationality.schema.ts
@@ -0,0 +1,18 @@
+import { createZodDto } from "nestjs-zod";
+
+import { z } from "zod";
+
+export const createNationalitySchema = z.object({
+  name: z.string().min(1).describe("Nama kewarganegaraan"),
+  description: z.string().optional().describe("Deskripsi kewarganegaraan"),
+});
+
+export const updateNationalitySchema = createNationalitySchema.partial();
+
+export class CreateNationalityDto extends createZodDto(
+  createNationalitySchema,
+) {}
+
+export class UpdateNationalityDto extends createZodDto(
+  updateNationalitySchema,
+) {}
