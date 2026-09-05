f48b850f2276903ab1e8dc5e652a5911f0a7b76d
Hilmy Zaky Ramadhan <hilmyzaky028@gmail.com>
2026-09-05 11:15:28 +0700

    feat(shared): export files announcements and notifications entities

--- packages/shared/src/entities/index.ts
+++ packages/shared/src/entities/index.ts
@@ -22,3 +22,6 @@ export * from "#entities/study-result.entity";
 export * from "#entities/academic-year.entity";
 export * from "#entities/academic-status.entity";
 export * from "#entities/employment-status.entity";
+export * from "#entities/file.entity";
+export * from "#entities/announcement.entity";
+export * from "#entities/notification.entity";
