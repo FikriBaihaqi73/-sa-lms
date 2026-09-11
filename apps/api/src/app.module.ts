5d4a664c9d8b01945b936804b16e3cbdfb2b04e1
paes18 <faiztio28@gmail.com>
2026-09-11 13:20:56 +0700

    feat: implement CRUD for religions and global auth configuration

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -9,8 +9,8 @@ import { AttendanceStatusModule } from "./attendance-status/attendance-status.mo
 import { AuthModule } from "./auth/auth.module";
 import { JwtAuthGuard } from "./auth/jwt-auth.guard";
 import { EmploymentStatusModule } from "./employment-status/employment-status.module";
-import { InstitutionLevelModule } from "./institution-level/institution-level.module";
 import { InstitutionModule } from "./institution/institution.module";
+import { InstitutionLevelModule } from "./institution-level/institution-level.module";
 import { LearningModuleModule } from "./module/module.module";
 import { ModuleContentModule } from "./module-content/module-content.module";
 import { PermissionModule } from "./permission/permission.module";
